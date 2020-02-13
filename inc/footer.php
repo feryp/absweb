3c2e5321331ccb3904d7335cac414a90e18e4b2f
Dwi Pamungkas <dw.pamungkas@gmail.com>
2020-02-13 16:10:24 +0700

    add first source

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.4.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script>
+
+//YOUR CODE HERE
+
+</script>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require 'inc/header.php'; ?>
+        <title>Home | My Site</title>
+    </head>
+    <body>
+    <?php require 'inc/top-menu.php'; ?>
+    <!--CONTENT -->
+
+
+
+
+
+    <!--END CONTENT -->
+    <?php require 'inc/footer.php'; ?>
+    </body>
+</html>
+
+    
